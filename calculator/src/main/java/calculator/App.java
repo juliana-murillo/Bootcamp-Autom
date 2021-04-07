28435324f03fac64ade298f1c7f21aa03567ffe7
Juliana Murillo Marmolejo <juliana.murillo@globant.com>
2021-04-07 18:23:51 -0500

    addition  and subtraction methods

--- calculator/src/main/java/calculator/App.java
+++ calculator/src/main/java/calculator/App.java
@@ -1,14 +1,13 @@
 package calculator;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         App calculator = new App();
+        System.out.println("Sum is: " + calculator.add(5, 8.5));
+        System.out.println("Substraction is: " + calculator.sub(5, 8.5));
         System.out.println("Multiplication is: " + calculator.mul(5, 8.5));
         System.out.println("Division is: " + calculator.div(5, 8.5));
     }
@@ -23,5 +22,18 @@ public class App
         double result;
         result = Num / Num2;
         return result;
+       
     }
+    public double add(double Num, double Num2){
+        double result;
+        result = Num + Num2;
+        return result;
+    }
+
+    public double sub(double Num, double Num2){
+        double result;
+        result = Num - Num2;
+        return result;
+    }
+    
 }
