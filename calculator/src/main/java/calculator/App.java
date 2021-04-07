dfba17f7aefd42fbb2fda9232f384422ab493e3e
Juliana Murillo Marmolejo <juliana.murillo@globant.com>
2021-04-07 18:26:08 -0500

    multiplication  and division methods

--- calculator/src/main/java/calculator/App.java
+++ calculator/src/main/java/calculator/App.java
@@ -9,5 +9,19 @@ public class App
     public static void main( String[] args )
     {
         App calculator = new App();
+        System.out.println("Multiplication is: " + calculator.mul(5, 8.5));
+        System.out.println("Division is: " + calculator.div(5, 8.5));
+    }
+
+    public double mul(double Num, double Num2){
+        double result;
+        result = Num * Num2;
+        return result;
+    }
+
+    public double div(double Num, double Num2){
+        double result;
+        result = Num / Num2;
+        return result;
     }
 }
